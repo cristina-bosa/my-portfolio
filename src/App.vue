18e1cb238942869f99b6d5643bbab5bd75ea9680
Cristina Bosa <cristinabosasanchez@gmail.com>
2021-09-03 13:03:31 +0100

    Cambios en diseño

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,32 @@
 <template>
-  <header class="h-screen flex justify-center justify-items-center">
-    <nav>
+  <header :class="isDark ? 'dark' : ''">
+    <nav class="p-3 m-0 w-full flex flex-row flex-wrap bg-white dark:bg-gray-800">
+      <div class="w-1/5 flex flex-row flex-wrap justify-center content-center">
+        <h2>Cristina</h2>
+      </div>
+
+      <div id="nav-items" class="sm:border-l-4 md:border-none border-indigo-400 transition close sm:pt-16 md:pt-0 sm:h-full md:h-auto sm:absolute sm:right-0 sm:top-0 md:top-auto md:right-auto md:relative md:w-4/5 sm:bg-gray-900 sm:text-white md:text-gray-500 md:bg-transparent md:flex flex flex-row flex-wrap justify-center sm:content-start md:content-center">
       <button v-if="isDark === true" @click="isDark = !isDark">
         <SunIcon class="h-5 w-5 text-black-700" />
       </button>
       <button v-else @click="isDark = !isDark">
         <MoonIcon class="h-5 w-5 text-black-700" />
       </button>
+  </div>
+
     </nav>
 
-    <h1 class="text-center text-5xl text-white">
-      Designer & Front-end developer
-    </h1>
+      <div class="h-screen flex justify-center items-center">
+        <h1 class="text-center font-extrabold text-5xl text-white">Designer & Front-end developer</h1>
+        <a href="#presentation" class="text-center"><ArrowNarrowDownIcon class="h-5 w-5 text-black-700" /></a>
+      </div>
+
   </header>
   <main :class="isDark ? 'dark' : ''">
     <div class="dark:bg-gray-800">
       <div class="container mx-auto">
-        <section class="pt-40 pb-40">
-          <h2 class="text-2xl text-blue-400 text-center mb-5 dark:text-white">Hola, soy Cristina. Encantada de conocerte</h2>
+        <section class="pt-40 pb-40" id="presentation">
+          <h2 class="text-2xl text-blue-400 font-bold text-center mb-5 dark:text-white">Hola, soy Cristina. Encantada de conocerte</h2>
           <p class="text-gray-800 text-center dark:text-gray-400">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem
             quod reiciendis perferendis nesciunt eius! Possimus eos, illum,
@@ -63,12 +72,13 @@
 
 <script>
 import "./assets/tailwind.css";
-import { MoonIcon, SunIcon } from "@heroicons/vue/outline";
+import { MoonIcon, SunIcon, ArrowNarrowDownIcon } from "@heroicons/vue/outline";
 export default {
   name: "App",
   components: {
     MoonIcon,
     SunIcon,
+    ArrowNarrowDownIcon
   },
   data() {
     return {
@@ -80,7 +90,7 @@ export default {
 
 <style>
 header {
-  background-image: url("https://images.pexels.com/photos/2471235/pexels-photo-2471235.jpeg?cs=srgb&dl=pexels-luis-quintero-2471235.jpg&fm=jpg");
+  background-image: url("https://images.pexels.com/photos/5186869/pexels-photo-5186869.jpeg?cs=srgb&dl=pexels-fiona-art-5186869.jpg&fm=jpg");
   background-repeat: no-repeat;
   background-size: cover;
 }
